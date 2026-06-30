4b66548d2bd4e8f17f6ee8d642ef155662611566
dyalamuhammad <dyalamuhammad@gmail.com>
2026-06-30 11:24:18 +0700

    fix download open blank

--- app/actions/download.ts
+++ app/actions/download.ts
@@ -5,7 +5,9 @@ import { supabaseAdmin } from "@/lib/supabase/server";
 export async function getDownloadUrl(path: string) {
   const { data, error } = await supabaseAdmin.storage
     .from("uploads")
-    .createSignedUrl(path, 60); // berlaku 60 detik
+    .createSignedUrl(path, 60, {
+        download: true,
+    }); // berlaku 60 detik
 
   if (error) {
     throw error;
